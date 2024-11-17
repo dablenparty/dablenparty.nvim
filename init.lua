447b30b145ded60c3d86328f19a8bbae396b108f
dablenparty <hdavenport8902@gmail.com>
2024-11-16 23:52:49 -0500

    Add nvim-tree, nvim-drawer, flutter-tools plugins

--- init.lua
+++ init.lua
@@ -1,88 +1,6 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
+-- NOTE: from: https://github.com/nvim-tree/nvim-tree.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
@@ -324,6 +242,84 @@ require('lazy').setup({
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
     },
+  },
+  -- File finder that replaces the default nvim UI for navigating directories
+  -- Also used for the drawer
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    version = '*',
+    lazy = false,
+    config = function()
+      require('nvim-tree').setup {}
+    end,
+  },
+
+  {
+    'mikew/nvim-drawer',
+    opts = {},
+    config = function(_, opts)
+      local drawer = require 'nvim-drawer'
+      drawer.setup(opts)
+
+      drawer.create_drawer {
+        -- This is needed for nvim-tree.
+        nvim_tree_hack = true,
+
+        -- Position on the right size of the screen.
+        position = 'right',
+        size = 40,
+
+        -- Alternatively, you can have it floating.
+        -- size = 40,
+        -- position = 'float',
+        -- win_config = {
+        --   margin = 2,
+        --   border = 'rounded',
+        --   anchor = 'CE',
+        --   width = 40,
+        --   height = '80%',
+        -- },
+
+        on_vim_enter = function(event)
+          --- Open the drawer on startup.
+          event.instance.open {
+            focus = false,
+          }
+
+          --- Example mapping to toggle.
+          vim.keymap.set('n', '<leader>e', function()
+            event.instance.focus_or_toggle()
+          end)
+        end,
+
+        --- Ideally, we would just call this here and be done with it, but
+        --- mappings in nvim-tree don't seem to apply when re-using a buffer in
+        --- a new tab / window.
+        on_did_create_buffer = function()
+          local nvim_tree_api = require 'nvim-tree.api'
+          nvim_tree_api.tree.open { current_window = true }
+        end,
+
+        --- This gets the tree to sync when changing tabs.
+        on_did_open = function()
+          local nvim_tree_api = require 'nvim-tree.api'
+          nvim_tree_api.tree.reload()
+
+          vim.opt_local.number = false
+          vim.opt_local.signcolumn = 'no'
+          vim.opt_local.statuscolumn = ''
+        end,
+
+        --- Cleans up some things when closing the drawer.
+        on_did_close = function()
+          local nvim_tree_api = require 'nvim-tree.api'
+          nvim_tree_api.tree.close()
+        end,
+      }
+    end,
   },
 
   -- NOTE: Plugins can specify dependencies.
@@ -675,6 +671,21 @@ require('lazy').setup({
       }
     end,
   },
+  {
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = function()
+      require('flutter-tools').setup {
+        decorations = {
+          device = true,
+        },
+      }
+    end,
+  },
 
   { -- Autoformat
     'stevearc/conform.nvim',
@@ -939,7 +950,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
