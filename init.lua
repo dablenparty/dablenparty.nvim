1c8d1e1f8a5aa6eed8e19df15a3a731ecadf435e
dablenparty <hdavenport8902@gmail.com>
2024-12-15 01:17:19 -0500

    Copied gitsign config from kickstart dir

--- init.lua
+++ init.lua
@@ -150,18 +150,6 @@ require('lazy').setup({
 
   -- Use `opts = {}` to force a plugin to be loaded.
   -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
 
   -- NOTE: Plugins can specify dependencies.
   --
