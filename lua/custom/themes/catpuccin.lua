c8b6c59216c26d083a7b054dcfc820678ef41b35
dablenparty <hdavenport8902@gmail.com>
2024-12-15 01:07:37 -0500

    Only enable one color scheme at a time

--- lua/custom/themes/catpuccin.lua
+++ lua/custom/themes/catpuccin.lua
@@ -2,9 +2,6 @@ return {
   'catppuccin/nvim',
   name = 'catppuccin',
   priority = 1000,
-  init = function()
-    vim.cmd.colorscheme 'catppuccin-mocha'
-  end,
   opts = {
     default_integrations = false,
     integrations = {
--- lua/custom/themes/tokyonight.lua
+++ lua/custom/themes/tokyonight.lua
@@ -2,7 +2,7 @@ return {
   'folke/tokyonight.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
-    vim.cmd.colorscheme 'tokyonight-night'
+    -- vim.cmd.colorscheme 'tokyonight-night'
 
     -- You can configure highlights by doing something like:
     vim.cmd.hi 'Comment gui=none'
