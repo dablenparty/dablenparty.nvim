2c3776f2aac80f718896723199d141477084075e
dablenparty <hdavenport8902@gmail.com>
2024-12-15 02:36:34 -0500

    Add telescope binds to search current buffer, treesitter

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -36,6 +36,9 @@ return {
         live_grep = {
           theme = 'ivy',
         },
+        current_buffer_fuzzy_find = {
+          theme = 'ivy',
+        },
       },
       extensions = {
         ['ui-select'] = {
@@ -50,6 +53,7 @@ return {
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
+    vim.keymap.set('n', '<leader>s%', builtin.current_buffer_fuzzy_find, { desc = 'Fuzzy [s]earch current buffer' })
     vim.keymap.set('n', '<leader>sc', builtin.colorscheme, { desc = '[S]earch [C]olorschemes' })
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
@@ -59,6 +63,7 @@ return {
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+    vim.keymap.set('n', '<leader>st', builtin.treesitter, { desc = '[S]earch [T]reesitter' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
