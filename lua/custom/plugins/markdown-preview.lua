0a131864852e7f9e8c54c847e7cdc1bca60449fd
dablenparty <hdavenport8902@gmail.com>
2024-12-18 14:11:15 -0500

    Refactor markdown-preview to install with yarn
    Using NPM is technically supported, but it changes the lock file and
    creates package-lock.json, which then makes Lazy complain about local
    changes

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -1,7 +1,7 @@
 return {
   'iamcco/markdown-preview.nvim',
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  build = 'cd app && npm install',
+  build = 'cd app && yarn install',
   init = function()
     vim.g.mkdp_auto_close = 0
     vim.g.mkdp_echo_preview_url = 1
