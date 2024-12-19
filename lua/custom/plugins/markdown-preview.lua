b53849bd3e349ca6c8e28ceba2c00893c265daf2
dablenparty <hdavenport8902@gmail.com>
2024-12-18 23:49:27 -0500

    Refactor markdown keymaps to load on LspAttach
    The keymaps are only available when a markdown file is open

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -8,12 +8,17 @@ return {
     vim.g.mkdp_filetypes = { 'markdown' }
   end,
   config = function()
-    vim.keymap.set('n', '<leader>mp', function()
-      vim.cmd 'MarkdownPreview'
-    end, { desc = '[M]arkdown [P]review' })
-    vim.keymap.set('n', '<leader>ms', function()
-      vim.cmd 'MarkdownPreviewStop'
-    end, { desc = '[M]arkdown [S]top Preview' })
+    vim.api.nvim_create_autocmd('LspAttach', {
+      pattern = { '*.md', '*.markdown' },
+      callback = function(ev)
+        vim.keymap.set('n', '<leader>mp', function()
+          vim.cmd 'MarkdownPreview'
+        end, { desc = '[M]arkdown [P]review' })
+        vim.keymap.set('n', '<leader>ms', function()
+          vim.cmd 'MarkdownPreviewStop'
+        end, { desc = '[M]arkdown [S]top Preview' })
+      end,
+    })
   end,
   ft = { 'markdown' },
 }
