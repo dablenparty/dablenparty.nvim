6b87ebca609cc52f2615a56233fa8180dd33e0c7
dablenparty <hdavenport8902@gmail.com>
2024-12-20 12:10:33 -0500

    Enable treesitter-, LSP-based textobjects

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -55,12 +55,64 @@ return {
         },
       },
       textobjects = {
+        select = {
+          enable = true,
+
+          -- Automatically jump forward to textobj, similar to targets.vim
+          lookahead = true,
+
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ['ac'] = { query = '@class.outer', desc = 'Select around class' },
+            ['ic'] = { query = '@class.inner', desc = 'Select inside class' },
+            ['af'] = { query = '@function.outer', desc = 'Select around function' },
+            ['if'] = { query = '@function.inner', desc = 'Select inside function' },
+            ['al'] = { query = '@conditional.outer', desc = 'Select around conditional' },
+            ['il'] = { query = '@conditional.inner', desc = 'Select inside conditional' },
+            ['ao'] = { query = '@comment.outer', desc = 'Select around comment' },
+            ['io'] = { query = '@comment.inner', desc = 'Select inside comment' },
+            -- You can also use captures from other query groups like `locals.scm`
+            ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
+          },
+          -- You can choose the select mode (default is charwise 'v')
+          --
+          -- Can also be a function which gets passed a table with the keys
+          -- * query_string: eg '@function.inner'
+          -- * method: eg 'v' or 'o'
+          -- and should return the mode ('v', 'V', or '<c-v>') or a table
+          -- mapping query_strings to modes.
+          selection_modes = {
+            ['@function.outer'] = 'V', -- linewise
+            ['@conditional.outer'] = 'V', -- linewise
+            ['@parameter.outer'] = 'v', -- charwise
+            ['@class.outer'] = 'V', -- blockwise
+          },
+          -- If you set this to `true` (default is `false`) then any textobject is
+          -- extended to include preceding or succeeding whitespace. Succeeding
+          -- whitespace has priority in order to act similarly to eg the built-in
+          -- `ap`.
+          --
+          -- Can also be a function which gets passed a table with the keys
+          -- * query_string: eg '@function.inner'
+          -- * selection_mode: eg 'v'
+          -- and should return true or false
+          include_surrounding_whitespace = false,
+        },
         move = {
           enable = true,
           goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner' },
           goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner' },
           goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner' },
           goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner' },
+        },
+        lsp_interop = {
+          enable = true,
+          border = 'none',
+          floating_preview_opts = { border = 'rounded' },
+          peek_definition_code = {
+            ['<leader>df'] = '@function.outer',
+            ['<leader>dc'] = '@class.outer',
+          },
         },
       },
     },
