a1a40d17ae119c6ee53a84635b0269c7d51d2fe6
dablenparty <hdavenport8902@gmail.com>
2024-12-30 21:23:50 -0500

    Add toggle for RenderMarkdown
    Added a Snacks toggle to enable/disable render-markdown

--- lua/custom/plugins/render-markdown.lua
+++ lua/custom/plugins/render-markdown.lua
@@ -5,4 +5,21 @@ return {
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = { render_modes = { 'n', 'c', 't' } },
+  config = function(_, opts)
+    require('render-markdown').setup(opts)
+    Snacks.toggle({
+      name = 'Render Markdown',
+      get = function()
+        return require('render-markdown.state').enabled
+      end,
+      set = function(enabled)
+        local m = require 'render-markdown'
+        if enabled then
+          m.enable()
+        else
+          m.disable()
+        end
+      end,
+    }):map '<leader>um'
+  end,
 }
