c48f41d875c564f562708e5c55c6566b15af8191
dablenparty <anoptimisticcouch@gmail.com>
2025-01-01 14:41:17 -0500

    Add blink.chartoggle plugin

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,78 +1,107 @@
 return {
-  'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = {
-    {
-      'L3MON4D3/LuaSnip',
-      -- follow latest release.
-      version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-      -- install jsregexp (optional!).
-      build = 'make install_jsregexp',
+  {
+    'saghen/blink.nvim',
+    keys = {
+      -- chartoggle
+      {
+        ';',
+        function()
+          require('blink.chartoggle').toggle_char_eol ';'
+        end,
+        mode = { 'n', 'v' },
+        desc = 'Toggle ; at EOL',
+      },
+      {
+        ',',
+        function()
+          require('blink.chartoggle').toggle_char_eol ','
+        end,
+        mode = { 'n', 'v' },
+        desc = 'Toggle , at EOL',
+      },
     },
-    {
-      'rafamadriz/friendly-snippets',
-      config = function()
-        require('luasnip.loaders.from_vscode').lazy_load()
-      end,
-    },
-    {
-      'saghen/blink.compat',
-      lazy = true,
+    -- all modules handle lazy loading internally
+    lazy = false,
+    opts = {
+      chartoggle = { enabled = true },
     },
   },
-
-  build = 'cargo +nightly build --release',
-
-  event = 'InsertEnter',
-
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = { preset = 'default', ['<C-e>'] = { 'hide', 'fallback' } },
-
-    appearance = {
-      -- will be removed in a future release
-      use_nvim_cmp_as_default = true,
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono',
+  {
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = {
+      {
+        'L3MON4D3/LuaSnip',
+        -- follow latest release.
+        version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = 'make install_jsregexp',
+      },
+      {
+        'rafamadriz/friendly-snippets',
+        config = function()
+          require('luasnip.loaders.from_vscode').lazy_load()
+        end,
+      },
+      {
+        'saghen/blink.compat',
+        lazy = true,
+      },
     },
 
-    snippets = {
-      expand = function(snippet)
-        require('luasnip').lsp_expand(snippet)
-      end,
-      active = function(filter)
-        if filter and filter.direction then
-          return require('luasnip').jumpable(filter.direction)
-        end
-        return require('luasnip').in_snippet()
-      end,
-      jump = function(direction)
-        require('luasnip').jump(direction)
-      end,
-    },
-    sources = {
-      default = { 'lsp', 'path', 'luasnip', 'buffer', 'dadbod', 'markdown' },
-      providers = {
-        dadbod = { name = 'Dadbod', module = 'vim_dadbod_completion.blink' },
-        markdown = { name = 'RenderMarkdown', module = 'render-markdown.integ.blink' },
-        obsidian = {
-          name = 'obsidian',
-          module = 'blink.compat.source',
-        },
-        obsidian_new = {
-          name = 'obsidian_new',
-          module = 'blink.compat.source',
-        },
-        obsidian_tags = {
-          name = 'obsidian_tags',
-          module = 'blink.compat.source',
+    build = 'cargo +nightly build --release',
+
+    event = 'InsertEnter',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = { preset = 'default', ['<C-e>'] = { 'hide', 'fallback' } },
+
+      appearance = {
+        -- will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono',
+      },
+
+      snippets = {
+        expand = function(snippet)
+          require('luasnip').lsp_expand(snippet)
+        end,
+        active = function(filter)
+          if filter and filter.direction then
+            return require('luasnip').jumpable(filter.direction)
+          end
+          return require('luasnip').in_snippet()
+        end,
+        jump = function(direction)
+          require('luasnip').jump(direction)
+        end,
+      },
+      sources = {
+        default = { 'lsp', 'path', 'luasnip', 'buffer', 'dadbod', 'markdown' },
+        providers = {
+          dadbod = { name = 'Dadbod', module = 'vim_dadbod_completion.blink' },
+          markdown = { name = 'RenderMarkdown', module = 'render-markdown.integ.blink' },
+          obsidian = {
+            name = 'obsidian',
+            module = 'blink.compat.source',
+          },
+          obsidian_new = {
+            name = 'obsidian_new',
+            module = 'blink.compat.source',
+          },
+          obsidian_tags = {
+            name = 'obsidian_tags',
+            module = 'blink.compat.source',
+          },
         },
       },
     },
+    -- allows extending the providers array elsewhere in your config
+    -- without having to redefine it
+    opts_extend = { 'sources.default' },
   },
-  -- allows extending the providers array elsewhere in your config
-  -- without having to redefine it
-  opts_extend = { 'sources.default' },
 }
