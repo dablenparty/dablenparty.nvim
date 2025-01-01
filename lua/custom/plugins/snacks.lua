689997c7da5b12e309db8b707c44e908daeac2ef
dablenparty <hdavenport8902@gmail.com>
2025-01-01 12:24:12 -0500

    Remove Neovim News bind
    It is not up to date and I don't really need it anyway

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -109,24 +109,6 @@ return {
       end,
       desc = 'which_key_ignore',
     },
-    {
-      '<leader>N',
-      desc = 'Neovim News',
-      function()
-        Snacks.win {
-          file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
-          width = 0.6,
-          height = 0.6,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = 'yes',
-            statuscolumn = ' ',
-            conceallevel = 3,
-          },
-        }
-      end,
-    },
   },
   init = function()
     vim.api.nvim_create_autocmd('User', {
