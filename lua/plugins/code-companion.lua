88398798376186bc1aaa6392cce5e9c5f5843ed9
dablenparty <anoptimisticcouch@gmail.com>
2025-01-06 03:44:19 -0500

    Implement Ollama for codecompanion

--- lua/plugins/code-companion.lua
+++ lua/plugins/code-companion.lua
@@ -4,5 +4,40 @@ return {
     { 'nvim-lua/plenary.nvim', branch = 'master' },
     'nvim-treesitter/nvim-treesitter',
   },
-  opts = {},
+  config = function()
+    vim.g.codecompanion_adapter = 'ollama'
+    require('codecompanion').setup {
+      adapters = {
+        ollama = function()
+          return require('codecompanion.adapters').extend('ollama', {
+            schema = {
+              model = {
+                default = 'qwen2.5-coder:7b',
+              },
+            },
+            env = {
+              url = function()
+                local url = os.getenv 'OLLAMA_URL'
+                if url == nil then
+                  vim.notify('OLLAMA_URL is not set, codecompanion will not work', vim.log.levels.WARN)
+                else
+                  return url
+                end
+              end,
+            },
+            headers = {
+              ['Content-Type'] = 'application/json',
+            },
+            parameters = {
+              sync = true,
+            },
+          })
+        end,
+      },
+      opts = {
+        -- I host it locally
+        allow_insecure = true,
+      },
+    }
+  end,
 }
