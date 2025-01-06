c5fa3bef4b2917b5c6866eed9d3b2badb7d572fb
dablenparty <anoptimisticcouch@gmail.com>
2025-01-06 02:50:04 -0500

    Add codecompanion plugin

---
+++ lua/plugins/code-companion.lua
@@ -0,0 +1,8 @@
+return {
+  'olimorris/codecompanion.nvim',
+  dependencies = {
+    { 'nvim-lua/plenary.nvim', branch = 'master' },
+    'nvim-treesitter/nvim-treesitter',
+  },
+  opts = {},
+}
--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,7 +1,7 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
-  ft = { 'markdown', 'quarto' },
+  ft = { 'codecompanion', 'markdown', 'quarto' },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = { render_modes = { 'n', 'c', 't' } },
