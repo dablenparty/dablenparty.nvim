2ce134527ffc87f00b034902d95aa2fcfc6f0951
dablenparty <hdavenport8902@gmail.com>
2025-01-07 15:22:50 -0500

    Refactor code-companion.lua to use opts
    The config function was long and confusing, extracting opts makes it
    more readable and a little more idiomatic imo

--- lua/plugins/code-companion.lua
+++ lua/plugins/code-companion.lua
@@ -11,40 +11,43 @@ return {
     { 'nvim-lua/plenary.nvim', branch = 'master' },
     'nvim-treesitter/nvim-treesitter',
   },
-  config = function()
+  opts = {
+    adapters = {
+      ollama = function()
+        return require('codecompanion.adapters').extend('ollama', {
+          schema = {
+            model = {
+              default = 'qwen2.5-coder:7b',
+            },
+          },
+          env = {
+            url = function()
+              local url = os.getenv 'OLLAMA_URL'
+              if url == nil then
+                vim.notify('Environment variable "OLLAMA_URL" not set, codecompanion will not work', vim.log.levels.ERROR, { title = 'codecompanion' })
+              end
+              return url
+            end,
+          },
+          headers = {
+            ['Content-Type'] = 'application/json',
+          },
+          parameters = {
+            sync = true,
+          },
+        })
+      end,
+    },
+    -- adapter opts
+    opts = {
+      -- I host it locally
+      allow_insecure = true,
+    },
+  },
+  config = function(_, opts)
+    opts = opts or {}
+    -- sets global provider so that I don't have to manually set it for each action
     vim.g.codecompanion_adapter = 'ollama'
-    require('codecompanion').setup {
-      adapters = {
-        ollama = function()
-          return require('codecompanion.adapters').extend('ollama', {
-            schema = {
-              model = {
-                default = 'qwen2.5-coder:7b',
-              },
-            },
-            env = {
-              url = function()
-                local url = os.getenv 'OLLAMA_URL'
-                if url == nil then
-                  vim.notify('OLLAMA_URL is not set, codecompanion will not work', vim.log.levels.WARN)
-                else
-                  return url
-                end
-              end,
-            },
-            headers = {
-              ['Content-Type'] = 'application/json',
-            },
-            parameters = {
-              sync = true,
-            },
-          })
-        end,
-      },
-      opts = {
-        -- I host it locally
-        allow_insecure = true,
-      },
-    }
+    require('codecompanion').setup(opts)
   end,
 }
