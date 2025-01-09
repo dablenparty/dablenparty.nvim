4c66b3763b1f31ebd1d9c39233508a024afe9eae
dablenparty <hdavenport8902@gmail.com>
2025-01-09 01:29:21 -0500

    Enable transparent background on mac in everforest theme
    Also switches to using init function and removes a comment

--- lua/themes/everforest.lua
+++ lua/themes/everforest.lua
@@ -1,16 +1,12 @@
 return {
   'sainnhe/everforest',
   priority = 1000,
-  config = function()
-    -- This theme is designed to be a native Vim plugin and uses Vim globals.
-    -- I designed this config function to make it "feel" more like a lazy.vim plugin.
-
+  init = function()
     -- add config here, without the "everforest_" prefix
     local opts = {
       enable_italic = true,
       background = 'hard',
-      -- TODO: see if this still blurs on macOS and use it if it does
-      -- transparent_background = 1,
+      transparent_background = vim.uv.os_uname().sysname == 'Darwin',
       float_style = 'dim',
       diagnostic_text_highlight = 1,
       diagnostic_virtual_text = 'highlight',
