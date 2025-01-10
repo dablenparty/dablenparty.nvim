e34f803d810b274af612717eb009270f2e694d87
dablenparty <hdavenport8902@gmail.com>
2025-01-10 13:12:38 -0500

    Implement markdown rendering for Rust comments
    render-markdown has builtin support for injecting itself into other
    languages with treesitter queries, so I wrote some for Rust line/block
    comments

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,10 +1,31 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
-  ft = { 'codecompanion', 'markdown', 'quarto' },
+  -- make sure to include injected languages here too
+  ft = { 'codecompanion', 'markdown', 'quarto', 'rust' },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = { render_modes = { 'n', 'c', 't' } },
+  opts = {
+    render_modes = { 'n', 'c', 't' },
+    injections = {
+      rust = {
+        enabled = true,
+        query = [[
+          ((line_comment (doc_comment) @injection.content
+              (#set! injection.language "markdown")
+              (#set! injection.include-children)))
+
+          ((line_comment) @injection.content
+              (#set! injection.language "markdown")
+              (#set! injection.include-children))
+
+          ((block_comment) @injection.content
+              (#set! injection.language "markdown")
+              (#set! injection.include-children))
+        ]],
+      },
+    },
+  },
   config = function(_, opts)
     require('render-markdown').setup(opts)
     Snacks.toggle({
