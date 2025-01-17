c0888aa6d8720702c99f7ced5f44032c50895f20
dablenparty <hdavenport8902@gmail.com>
2025-01-16 23:07:22 -0500

    Fix missing vim.print definition
    I remember commenting this out, but not why...

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -115,12 +115,13 @@ return {
       pattern = 'VeryLazy',
       callback = function()
         -- Setup some globals for debugging (lazy-loaded)
-        -- _G.dd = function(...)
-        --   Snacks.debug.inspect(...)
-        -- end
+        _G.dd = function(...)
+          Snacks.debug.inspect(...)
+        end
         _G.bt = function()
           Snacks.debug.backtrace()
         end
+        vim.print = _G.dd -- Override print to use snacks for `:=` command
 
         -- Create some toggle mappings
         Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
