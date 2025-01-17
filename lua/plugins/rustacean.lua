64c5313836aab5c2ce2e0af23c213335584bc982
dablenparty <hdavenport8902@gmail.com>
2025-01-17 12:47:28 -0500

    Clean rustacean.nvim up a little

--- lua/plugins/rustacean.lua
+++ lua/plugins/rustacean.lua
@@ -3,18 +3,22 @@ return {
   version = '^5',
   lazy = false, -- this plugin handles laziness itself
   config = function()
-    local group = vim.api.nvim_create_augroup('rustaceanvim-lsp-attach', { clear = true })
     vim.api.nvim_create_autocmd({ 'LspAttach' }, {
       pattern = { '*.rs', 'Cargo.toml' },
-      group = group,
-      callback = function(_)
-        local cur_bufnr = vim.api.nvim_get_current_buf()
-        vim.keymap.set('n', '<leader>ca', function()
-          vim.cmd.RustLsp 'codeAction'
-        end, { desc = '[C]ode [A]ction', silent = true, buffer = cur_bufnr })
-        vim.keymap.set('n', 'K', function()
-          vim.cmd.RustLsp { 'hover', 'actions' }
-        end, { desc = 'Hover Menu', silent = true, buffer = cur_bufnr })
+      group = vim.api.nvim_create_augroup('rustaceanvim-lsp-attach', { clear = true }),
+      callback = function(event)
+        ---@param lhs string
+        ---@param rhs string
+        ---@param opts vim.api.keyset.keymap?
+        local set_key = function(lhs, rhs, opts)
+          opts = opts or {}
+          local default_opts = { buffer = event.buf, silent = true }
+
+          vim.api.nvim_set_keymap('n', lhs, rhs, vim.tbl_deep_extend('force', default_opts, opts))
+        end
+
+        set_key('<leader>ca', '<cmd>RustLsp codeAction<CR>', { desc = '[C]ode [A]ction' })
+        set_key('K', '<cmd>RustLsp hover actions', { desc = 'Hover Menu' })
       end,
     })
   end,
