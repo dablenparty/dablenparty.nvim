54997660a3f578f3d493b40e058b7d0dde732df3
dablenparty <anoptimisticcouch@gmail.com>
2025-01-18 12:21:05 -0500

    fix treesitter.lua formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,121 +1,119 @@
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    keys = {
-      { '<c-space>', desc = 'Increment Selection' },
-      { '<bs>', desc = 'Decrement Selection', mode = 'x' },
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+  keys = {
+    { '<c-space>', desc = 'Increment Selection' },
+    { '<bs>', desc = 'Decrement Selection', mode = 'x' },
+  },
+  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+  opts = {
+    ensure_installed = {
+      'bash',
+      'c',
+      'diff',
+      'dockerfile',
+      'html',
+      'javascript',
+      'jsdoc',
+      'json',
+      'jsonc',
+      'lua',
+      'luadoc',
+      'luap',
+      'markdown',
+      'markdown_inline',
+      'printf',
+      'python',
+      'query',
+      'regex',
+      'rust',
+      'toml',
+      'tsx',
+      'typescript',
+      'vim',
+      'vimdoc',
+      'xml',
+      'yaml',
     },
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = {
-        'bash',
-        'c',
-        'diff',
-        'dockerfile',
-        'html',
-        'javascript',
-        'jsdoc',
-        'json',
-        'jsonc',
-        'lua',
-        'luadoc',
-        'luap',
-        'markdown',
-        'markdown_inline',
-        'printf',
-        'python',
-        'query',
-        'regex',
-        'rust',
-        'toml',
-        'tsx',
-        'typescript',
-        'vim',
-        'vimdoc',
-        'xml',
-        'yaml',
+    -- Autoinstall languages that are not installed
+    auto_install = true,
+    highlight = {
+      enable = true,
+      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+      --  If you are experiencing weird indenting issues, add the language to
+      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+      additional_vim_regex_highlighting = { 'ruby' },
+    },
+    indent = { enable = true, disable = { 'ruby' } },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<C-space>',
+        node_incremental = '<C-space>',
+        scope_incremental = false,
+        node_decremental = '<bs>',
       },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-      incremental_selection = {
-        enable = true,
+    },
+    textobjects = {
+      select = {
+        -- handled by mini.ai
+        enable = false,
+
+        -- Automatically jump forward to textobj, similar to targets.vim
+        lookahead = true,
+
         keymaps = {
-          init_selection = '<C-space>',
-          node_incremental = '<C-space>',
-          scope_incremental = false,
-          node_decremental = '<bs>',
+          -- You can use the capture groups defined in textobjects.scm
+          ['ac'] = { query = '@class.outer', desc = 'Select around class' },
+          ['ic'] = { query = '@class.inner', desc = 'Select inside class' },
+          ['af'] = { query = '@function.outer', desc = 'Select around function' },
+          ['if'] = { query = '@function.inner', desc = 'Select inside function' },
+          ['al'] = { query = '@conditional.outer', desc = 'Select around conditional' },
+          ['il'] = { query = '@conditional.inner', desc = 'Select inside conditional' },
+          ['ao'] = { query = '@comment.outer', desc = 'Select around comment' },
+          ['io'] = { query = '@comment.inner', desc = 'Select inside comment' },
+          -- You can also use captures from other query groups like `locals.scm`
+          ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
         },
+        -- You can choose the select mode (default is charwise 'v')
+        --
+        -- Can also be a function which gets passed a table with the keys
+        -- * query_string: eg '@function.inner'
+        -- * method: eg 'v' or 'o'
+        -- and should return the mode ('v', 'V', or '<c-v>') or a table
+        -- mapping query_strings to modes.
+        selection_modes = {
+          ['@function.outer'] = 'V', -- linewise
+          ['@conditional.outer'] = 'V', -- linewise
+          ['@parameter.outer'] = 'v', -- charwise
+          ['@class.outer'] = 'V', -- blockwise
+        },
+        -- If you set this to `true` (default is `false`) then any textobject is
+        -- extended to include preceding or succeeding whitespace. Succeeding
+        -- whitespace has priority in order to act similarly to eg the built-in
+        -- `ap`.
+        --
+        -- Can also be a function which gets passed a table with the keys
+        -- * query_string: eg '@function.inner'
+        -- * selection_mode: eg 'v'
+        -- and should return true or false
+        include_surrounding_whitespace = false,
       },
-      textobjects = {
-        select = {
-          -- handled by mini.ai
-          enable = false,
-
-          -- Automatically jump forward to textobj, similar to targets.vim
-          lookahead = true,
-
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ['ac'] = { query = '@class.outer', desc = 'Select around class' },
-            ['ic'] = { query = '@class.inner', desc = 'Select inside class' },
-            ['af'] = { query = '@function.outer', desc = 'Select around function' },
-            ['if'] = { query = '@function.inner', desc = 'Select inside function' },
-            ['al'] = { query = '@conditional.outer', desc = 'Select around conditional' },
-            ['il'] = { query = '@conditional.inner', desc = 'Select inside conditional' },
-            ['ao'] = { query = '@comment.outer', desc = 'Select around comment' },
-            ['io'] = { query = '@comment.inner', desc = 'Select inside comment' },
-            -- You can also use captures from other query groups like `locals.scm`
-            ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
-          },
-          -- You can choose the select mode (default is charwise 'v')
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * method: eg 'v' or 'o'
-          -- and should return the mode ('v', 'V', or '<c-v>') or a table
-          -- mapping query_strings to modes.
-          selection_modes = {
-            ['@function.outer'] = 'V', -- linewise
-            ['@conditional.outer'] = 'V', -- linewise
-            ['@parameter.outer'] = 'v', -- charwise
-            ['@class.outer'] = 'V', -- blockwise
-          },
-          -- If you set this to `true` (default is `false`) then any textobject is
-          -- extended to include preceding or succeeding whitespace. Succeeding
-          -- whitespace has priority in order to act similarly to eg the built-in
-          -- `ap`.
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * selection_mode: eg 'v'
-          -- and should return true or false
-          include_surrounding_whitespace = false,
-        },
-        move = {
-          enable = true,
-          goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner' },
-          goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner' },
-          goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner' },
-          goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner' },
-        },
-        lsp_interop = {
-          enable = true,
-          border = 'none',
-          floating_preview_opts = { border = 'rounded' },
-          peek_definition_code = {
-            ['<leader>df'] = '@function.outer',
-            ['<leader>dc'] = '@class.outer',
-          },
+      move = {
+        enable = true,
+        goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner' },
+        goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner' },
+        goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner' },
+        goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner' },
+      },
+      lsp_interop = {
+        enable = true,
+        border = 'none',
+        floating_preview_opts = { border = 'rounded' },
+        peek_definition_code = {
+          ['<leader>df'] = '@function.outer',
+          ['<leader>dc'] = '@class.outer',
         },
       },
     },
