340a9d3ac64d7d4fcb2b5900a5dda414f7fd6c54
dablenparty <anoptimisticcouch@gmail.com>
2025-01-18 12:54:03 -0500

    Add mini.operators

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -24,6 +24,58 @@ return {
     },
   },
   {
+    'echasnovski/mini.operators',
+    event = 'VeryLazy',
+    opts = {
+      -- No need to copy this inside `setup()`. Will be used automatically.
+      {
+        -- Each entry configures one operator.
+        -- `prefix` defines keys mapped during `setup()`: in Normal mode
+        -- to operate on textobject and line, in Visual - on selection.
+
+        -- Evaluate text and replace with output
+        evaluate = {
+          prefix = '<leader>g=',
+
+          -- Function which does the evaluation
+          func = nil,
+        },
+
+        -- Exchange text regions
+        exchange = {
+          prefix = '<leader>gx',
+
+          -- Whether to reindent new text to match previous indent
+          reindent_linewise = true,
+        },
+
+        -- Multiply (duplicate) text
+        multiply = {
+          prefix = '<leader>gm',
+
+          -- Function which can modify text before multiplying
+          func = nil,
+        },
+
+        -- Replace text with register
+        replace = {
+          prefix = '<leader>gr',
+
+          -- Whether to reindent new text to match previous indent
+          reindent_linewise = true,
+        },
+
+        -- Sort text
+        sort = {
+          prefix = '<leader>gs',
+
+          -- Function which does the sort
+          func = nil,
+        },
+      },
+    },
+  },
+  {
     'echasnovski/mini.pairs',
     event = 'VeryLazy',
     opts = {
