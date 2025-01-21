fc4fa150b2063c82eb3abf487f8955dadcb778b4
dablenparty <anoptimisticcouch@gmail.com>
2025-01-21 12:21:39 -0500

    Refactor how Rustacean assigns keybinds
    It has an on_attach function built in! No need for an autocmd

--- lua/plugins/rustacean.lua
+++ lua/plugins/rustacean.lua
@@ -2,20 +2,34 @@ return {
   'mrcjkb/rustaceanvim',
   version = '^5',
   lazy = false, -- this plugin handles laziness itself
-  config = function()
-    vim.api.nvim_create_autocmd({ 'LspAttach' }, {
-      pattern = { '*.rs', 'Cargo.toml' },
-      group = vim.api.nvim_create_augroup('rustaceanvim-lsp-attach', { clear = true }),
-      callback = function(event)
-        local set_key = function(lhs, rhs, desc)
-          local opts = { buffer = event.buf, silent = true, desc = desc }
+  init = function()
+    ---@module 'rustaceanvim'
+    ---@type rustaceanvim.Opts
+    vim.g.rustaceanvim = {
+      ---@type rustaceanvim.lsp.ClientOpts
+      server = {
+        on_attach = function(_, bufnr)
+          local set_key = function(lhs, rhs, desc)
+            local opts = { buffer = bufnr, silent = true, desc = desc }
 
-          vim.keymap.set('n', lhs, rhs, opts)
-        end
+            vim.keymap.set('n', lhs, rhs, opts)
+          end
 
-        set_key('<leader>ca', '<cmd>RustLsp codeAction<CR>', '[C]ode [A]ction')
-        set_key('K', '<cmd>RustLsp hover actions<CR>', 'Hover Menu')
-      end,
-    })
+          -- overwrite LSP code actions and hover menu
+          set_key('<leader>ca', '<cmd>RustLsp codeAction<CR>', '[C]ode [A]ction')
+          set_key('K', '<cmd>RustLsp hover actions<CR>', 'Hover Menu')
+
+          -- useful diagnostic binds
+          set_key('<leader>xd', '<cmd>RustLsp renderDiagnostic current', 'Render [D]iagnostic')
+          set_key('<leader>xD', '<cmd>RustLsp renderDiagnostic cycle', 'Render Next [D]iagnostic')
+          set_key('<leader>xe', '<cmd>RustLsp explainError current', 'E[x]plain [E]rror on line')
+          set_key('<leader>xE', '<cmd>RustLsp explainError cycle', 'E[x]plain Next [E]rror')
+          set_key('<leader>xr', '<cmd>RustLsp relatedDiagnostics', '[R]elated Diagnostics')
+
+          -- misc
+          set_key('gx', '<cmd>RustLsp openDocs', 'Open docs.rs for the symbol under the cursor.')
+        end,
+      },
+    }
   end,
 }
