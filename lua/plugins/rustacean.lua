f9ca2b42b5c5e3887303697c557deade57ab47fe
dablenparty <anoptimisticcouch@gmail.com>
2025-01-21 15:30:01 -0500

    Update rustacean binds
    Add binds for rendering diags, opening docs.rs, and a better hover menu

--- lua/plugins/rustacean.lua
+++ lua/plugins/rustacean.lua
@@ -9,25 +9,26 @@ return {
       ---@type rustaceanvim.lsp.ClientOpts
       server = {
         on_attach = function(_, bufnr)
-          local set_key = function(lhs, rhs, desc)
+          local set_key = function(mode, lhs, rhs, desc)
             local opts = { buffer = bufnr, silent = true, desc = desc }
-
-            vim.keymap.set('n', lhs, rhs, opts)
+            vim.keymap.set(mode, lhs, rhs, opts)
           end
 
           -- overwrite LSP code actions and hover menu
-          set_key('<leader>ca', '<cmd>RustLsp codeAction<CR>', '[C]ode [A]ction')
-          set_key('K', '<cmd>RustLsp hover actions<CR>', 'Hover Menu')
+          -- enable in visual and select modes for grouped actions
+          set_key({ 'n', 'x' }, '<leader>ca', '<cmd>RustLsp codeAction<CR>', '[C]ode [A]ction')
+          set_key('n', 'K', '<cmd>RustLsp hover actions<CR>', 'Hover Menu')
+          set_key('x', 'K', '<cmd>RustLsp hover range<CR>', 'Hover Menu')
 
           -- useful diagnostic binds
-          set_key('<leader>xd', '<cmd>RustLsp renderDiagnostic current', 'Render [D]iagnostic')
-          set_key('<leader>xD', '<cmd>RustLsp renderDiagnostic cycle', 'Render Next [D]iagnostic')
-          set_key('<leader>xe', '<cmd>RustLsp explainError current', 'E[x]plain [E]rror on line')
-          set_key('<leader>xE', '<cmd>RustLsp explainError cycle', 'E[x]plain Next [E]rror')
-          set_key('<leader>xr', '<cmd>RustLsp relatedDiagnostics', '[R]elated Diagnostics')
+          set_key('n', '<leader>xd', '<cmd>RustLsp renderDiagnostic current<CR>', 'Render [D]iagnostic')
+          set_key('n', '<leader>xD', '<cmd>RustLsp renderDiagnostic cycle<CR>', 'Render Next [D]iagnostic')
+          set_key('n', '<leader>xe', '<cmd>RustLsp explainError current<CR>', 'E[x]plain [E]rror on line')
+          set_key('n', '<leader>xE', '<cmd>RustLsp explainError cycle<CR>', 'E[x]plain Next [E]rror')
+          set_key('n', '<leader>xr', '<cmd>RustLsp relatedDiagnostics<CR>', '[R]elated Diagnostics')
 
           -- misc
-          set_key('gx', '<cmd>RustLsp openDocs', 'Open docs.rs for the symbol under the cursor.')
+          set_key('n', 'gx', '<cmd>RustLsp openDocs<CR>', 'Open docs.rs for the symbol under the cursor.')
         end,
       },
     }
